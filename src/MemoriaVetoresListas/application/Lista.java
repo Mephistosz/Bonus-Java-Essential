037801992b7920935dde3056dc1733865b3560b4
felipe Gomes <Felipe.fg1@outlook.com>
2023-06-04 04:02:43 -0300

    Memoria, Vetores, Listas

---
+++ src/MemoriaVetoresListas/application/Lista.java
@@ -0,0 +1,54 @@
+package MemoriaVetoresListas.application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Lista {
+  public static void main(String[] args) {
+
+    List<String> list = new ArrayList<>();
+
+    final String separador = "---------------------";
+
+    list.add("Maria");
+    list.add("Alex");
+    list.add("Bob");
+    list.add("Anna");
+    list.add(2, "Marco");
+
+    System.out.println(list.size());
+
+    for (String x : list) {
+      System.out.println(x);
+    }
+
+    System.out.println(separador);
+
+    list.removeIf(x -> x.charAt(0) == 'M');
+
+    for (String x : list) {
+      System.out.println(x);
+    }
+
+    System.out.println(separador);
+
+    System.out.println("Index of Bob: " + list.indexOf("Bob"));
+    System.out.println("Index of Marco: " + list.indexOf("Marco"));
+
+    System.out.println(separador);
+
+    List<String> result = list.stream().filter(x -> x.charAt(0) == 'A').collect(Collectors.toList());
+
+    for (String x : result) {
+      System.out.println(x);
+    }
+
+    System.out.println(separador);
+
+    String name = list.stream().filter(x -> x.charAt(0) == 'J').findFirst().orElse(null);
+
+    System.out.println(name);
+  }
+
+}
---
+++ src/MemoriaVetoresListas/application/ProgramExercicio.java
@@ -0,0 +1,76 @@
+package MemoriaVetoresListas.application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import MemoriaVetoresListas.entities.Funcionario;
+
+public class ProgramExercicio {
+  public static void main(String[] args) {
+
+    Locale.setDefault(Locale.US);
+
+    Scanner sc = new Scanner(System.in);
+
+    Integer n;
+
+    System.out.print("How many employees will be registered? ");
+    n = sc.nextInt();
+
+    List<Funcionario> funcionario = new ArrayList<>();
+
+    for (int i = 1; i <= n; i++) {
+      System.out.printf("Emplyoee #%d:%n", i);
+
+      System.out.print("Id: ");
+      Integer id = sc.nextInt();
+      while (temId(funcionario, id)) {
+        System.out.println("Id já existente, Favor digitar outro");
+        System.out.print("Id: ");
+        id = sc.nextInt();
+      }
+
+      System.out.print("Name: ");
+      sc.nextLine();
+      String nome = sc.nextLine();
+
+      System.out.print("Salary: ");
+      Double salario = sc.nextDouble();
+
+      System.out.println("");
+
+      funcionario.add(new Funcionario(id, nome, salario));
+    }
+
+    System.out.print("Enter the employee id that will have salary increase : ");
+    Integer id = sc.nextInt();
+
+    Funcionario funcionarioAumento = funcionario.stream().filter(x -> x.getId().equals(id)).findFirst().orElse(null);
+
+    if (funcionarioAumento == null) {
+      System.out.println();
+      System.out.println("This id does not exist!");
+    } else {
+      System.out.print("Enter the percentage: ");
+      funcionarioAumento.increaseSalary(sc.nextDouble());
+      System.out.println();
+    }
+
+    System.out.println("List of employees:");
+    System.out.println();
+
+    for (Funcionario func : funcionario) {
+      System.out.println(func);
+    }
+
+    sc.close();
+
+  }
+
+  public static boolean temId(List<Funcionario> funcionario, Integer id) {
+    return funcionario.stream().filter(x -> x.getId().equals(id)).findFirst().orElse(null) != null;
+  }
+
+}
