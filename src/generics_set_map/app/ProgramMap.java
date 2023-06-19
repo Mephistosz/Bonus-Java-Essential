bb29ba3b234b2ca1778b63ebf4ed633ca0af1a61
felipe Gomes <Felipe.fg1@outlook.com>
2023-06-18 22:31:21 -0300

    correct the foreach to map

--- src/generics_set_map/app/ProgramMap.java
+++ src/generics_set_map/app/ProgramMap.java
@@ -1,30 +1,34 @@
 package generics_set_map.app;
 
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.TreeMap;
 
 public class ProgramMap {
   public static void main(String[] args) {
 
+    String email = "email";
+    String phone = "phone";
+
     Map<String, String> cookies = new TreeMap<>();
 
     cookies.put("username", "maria");
-    cookies.put("email", "maria@gmail.com");
-    cookies.put("phone", "99771122");
+    cookies.put(email, "maria@gmail.com");
+    cookies.put(phone, "99771122");
 
-    cookies.remove("email");
+    cookies.remove(email);
 
-    cookies.put("phone", "99771133");
+    cookies.put(phone, "99771133");
 
-    System.out.println("Contains 'phone' key: " + cookies.containsKey("phone"));
-    System.out.println("Phone number: " + cookies.get("phone"));
-    System.out.println("Email: " + cookies.get("email"));
+    System.out.println("Contains 'phone' key: " + cookies.containsKey(phone));
+    System.out.println("Phone number: " + cookies.get(phone));
+    System.out.println("Email: " + cookies.get(email));
     System.out.println("Size: " + cookies.size());
 
     System.out.println("ALL COOKIES:");
 
-    for (String key : cookies.keySet()) {
-      System.out.println(key + ": " + cookies.get(key));
+    for (Entry<String, String> x : cookies.entrySet()) {
+      System.out.println(x.getKey() + ": " + x.getValue());
     }
   }
 }
